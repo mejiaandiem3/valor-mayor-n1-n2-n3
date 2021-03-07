fca9d16a75156c0fd880cf1112b28920539998c7
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-07 01:57:45 -0600

    Agregar archivos de proyecto.

---
+++ valor mayor n1 n2 n3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valor_mayor_n1_n2_n3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double n1;
+            double n2;
+            double n3;
+
+            Console.WriteLine("Valor mayor.");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Escribe el valor del primer numero: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe el valor del segundo numero: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe el valor del tercer numero: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine("El numero " + n1 + " es mayor.");
+            }
+            else
+            {
+                if (n2 > n1 && n2 > n3)
+                {
+                    Console.WriteLine("El numero " + n2 + " es mayor.");
+                }
+                else
+                {
+                    Console.WriteLine("El numero " + n3 + " es mayor.");
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
